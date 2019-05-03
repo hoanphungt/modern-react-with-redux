5fa9d0f16b147cd3534616894e095d07f4d50e82
Hoan Phung <hoanphung.ptso1@gmail.com>
2019-05-03 20:06:17 +0200

    season display

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './SeasonDisplay.css';
+
+// export default class SeasonDisplay extends React.Component {
+//     render() {
+//         console.log(this.props)
+//         return <h1>Season Display</h1>
+//     }
+// }
+
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it is cold",
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season]
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left ${iconName} icon massive`} />
+            <h1>{text}</h1>
+            <i className={`icon-right ${iconName} icon massive`} />
+        </div>
+    )
+}
+
+export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    renderContent() {
+        if (this.state.lat === null && this.state.errorMessage === '') return <Loader message='Please allow access to your location' />;
+        if (this.state.errorMessage) return <h1>Error: {this.state.errorMessage}</h1>;
+        return (
+            <SeasonDisplay lat={this.state.lat} />
+        );
+    }
+
+    render() {
+        return <div className='border red'>{this.renderContent()}</div>
+    };
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+//constructor function from React.Component
+// constructor(props) {
+//     super(props);
+//     this.state = {
+//         lat: null
+//     };
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//             //use setState to update the local state
+//             this.setState({
+//                 lat: Math.floor(position.coords.latitude) 
+//             })
+//         },
+//         (err) => console.log(err)
+//     );
+// }
+
+// Challenge on updating Component
+// class Clock extends React.Component {
+//     state = {
+//         time: new Date().toLocaleTimeString()
+//     }
+
+//     componentDidMount() {
+//         //setInterval is a built-in function in JS that run the call-back function inside once in every interval that you have set
+//         setInterval(() => {
+//             this.setState({
+//                 time: new Date().toLocaleTimeString()
+//             })
+//         }, 1000)
+//     }
+
+//     render() {
+//         return (
+//             <div className='time'>
+//                 The time is: {this.state.time}
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <Clock />,
+//     document.querySelector('#root')
+// );
+
+
